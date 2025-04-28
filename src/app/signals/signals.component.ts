b2bb594c629815953a273ed19d584d0ffa525163
Mohamed Alaa <malaaosmanosman5260@gmail.com>
2025-04-28 16:06:49 +0300

    first commit

---
+++ src/app/signals/signals.component.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  templateUrl: './signals.component.html',
+  styleUrls: ['./signals.component.css']
+})
+export class SignalsComponent {
+  counter = signal(0);
+}
+
