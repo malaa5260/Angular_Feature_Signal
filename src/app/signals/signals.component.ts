f8d48da84530712a6754f69eac2c163cef878d8d
Mohamed Alaa <malaaosmanosman5260@gmail.com>
2025-04-28 20:08:49 +0300

    featues

--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -1,11 +1,33 @@
-import { Component, signal } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signals',
   templateUrl: './signals.component.html',
+  standalone: true,
+  imports: [NgFor],
   styleUrls: ['./signals.component.css']
 })
 export class SignalsComponent {
+  actions = signal<string[]>([]);
   counter = signal(0);
+  doublCounter = computed(() => this.counter() * 2);
+
+  constructor() {
+    effect(() => {
+      console.log('Counter changed:', this.counter());
+    })
+  }
+
+  increment() {
+    // this.counter.update(oldCounter => oldCounter + 1);
+    this.counter.set(this.counter() + 1);
+    this.actions.mutate(oldActions => oldActions.push('INCREMENT'));
+  }
+  decrement() {
+    this.counter.update(oldCounter => oldCounter - 1);
+    //this.actions.update(oldActions => [...oldActions, 'DECREMENT']);
+    this.actions.mutate(oldActions => oldActions.push('DECREMENT'));
+  }
 }
 
